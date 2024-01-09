e8c413198b81d15648d551b6acbfdf44a57d5496
Chia20-1 <chiachunlim97@gmail.com>
2024-01-09 15:21:05 +0800

    Update codes: Learnt Share Data in One Component

--- src/d2.jsx
+++ src/d2.jsx
@@ -1,22 +1,46 @@
 import React from "react"
+// Functional component need to do the import
+import { useState } from "react";
 import MyProfile from "./MyProfile"
 import ShoppingList from "./ShoppingList";
 
-function MyButton() {
-    return (
-        <button>
-            I'm a button!
-        </button>
-    )
-}
+// Props perimeter to fill it all
+function MyButton({count, onClick}){
+    return <button onClick={onClick}>{`Click ${count} times`}</button>
+} 
+
+
+
+// function MyButton() {
+//     // This decide is the component is separetely handled.
+//     // const[count, setCount] = useState(0);
+//     function handleClick(){
+//         // alert("Button is clicked.")
+//         setCount(count + 1);
+//     }
+//     return (
+//         <button onClick={handleClick}>
+//             {`Clicked ${count} times.`}
+//         </button>
+//     )
+// }
+
+// To share data between components, rather than saving the state in a separate function, integrate it into the export part.
 
 export function DayTwo() {
+    const [count, setCount] = useState(0);
+
+    function handleClick(){
+        setCount(count + 1);
+    }
+
     const isLoggedIn = true;
     return (
         <div>
             <h3>Welcome to my App on Day Two!</h3>
             {/* This is self closing tag */}
-            <MyButton/>
+            <MyButton count={count} onClick={handleClick}/>
+            <MyButton count={count} onClick={handleClick}/>
             {!isLoggedIn ? "" : <MyProfile/>}
             <ShoppingList/>
             
